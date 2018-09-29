f2780b73e40bb7c8a397c05010745293421602db
rvdavra <riteshdavra@gmail.com>
2018-09-29 14:46:26 +0530

    add layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'eight-puzzle';
+
+  pString = "A12345678_"
+
+  selectClass = "bg-primary"
+
+  first = null;
+  firstElement = null;
+
+  @ViewChildren('p') puzzle;
+
+  ngOnInit() {
+  }
+
+  puzzleClick(number) {
+    if (this.first) {
+      this.first = null;
+      this.resetBG();
+      this.closeEmpty();
+    } else {
+      this.first = number;
+      let puzzleArray: ElementRef[] = this.puzzle.toArray();
+      puzzleArray[+number - 1].nativeElement.classList.add(this.selectClass);
+      this.openEmpty();
+    }
+  }
+
+  resetBG() {
+    this.puzzle.toArray().forEach(element => {
+      element.nativeElement.classList.remove(this.selectClass);
+    });
+  }
+
+  openEmpty() {
+    let f = Math.floor((this.first - 1) / 3);
+    let up = f === 0 ? -1 : (this.first - 3);
+    let down = f === 2 ? -1 : (this.first + 3);
+    let left = ((this.first - 1) % 3) === 0 ? -1 : (this.first - 1);
+    let right = ((this.first - 1) % 3) === 2 ? -1 : (this.first + 1);
+    if (this.pString[up] === '_' || this.pString[down] === '_' || this.pString[left] === '_' || this.pString[right] === '_') {
+      this.pString = this.pString.replace('_', ' ');
+    }
+  }
+  closeEmpty() {
+    this.pString = this.pString.replace(' ', '_');
+  }
 }
